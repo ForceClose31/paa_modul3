567ee651f2c3f4780c5b0a833f6d4516b5250840
ForceClose31 <118113853+ForceClose31@users.noreply.github.com>
2024-03-31 21:49:31 +0700

    Modul 3

---
+++ PercobaanApi2/PercobaanApi2/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace PercobaanApi2.Models
+{
+    public class Mahasiswa
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+    }
+
+}
